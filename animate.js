adc775fde04c0dc5a3e029c34222bc25e7ca9a63
andrea <andrea.iccio@live.it>
2020-10-30 23:22:23 +0100

    dati aggiornati

---
+++ animate.js
@@ -0,0 +1,37 @@
+
+//oggetti che azionano l'effetto ad un elemento se con lo scroll diventa visibile
+function AnimateClass(array_el){
+		this.arrayel = arrayel;
+		this.animate=function(){
+			var i;
+			for(i=0; i<arrayel.length;i++)
+			$(arrayel[i].name_el).each(function() {
+				if (isScrolledIntoView(this) === true) {
+					$(this).addClass(arrayel[i].name_effect);
+				}
+			});
+		}
+}
+
+//oggetti che associano un elemento ad un effetto
+function AnimateElement(name_el, name_effect){
+		this.name_el=name_el;
+		this.name_effect=name_effect;
+}		
+    
+
+
+//funzione che determina se un elemento è visibile
+function isScrolledIntoView(elem)
+{
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
